1a73f74fbe7ec464e518b08c1ffa69a14c937b00
kusal374 <ms5377762@gmail.com>
2025-01-16 18:35:58 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://exgyekfwnztnaawaueuu.supabase.co'
-const SUPABASE_ANON_KEY = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const SUPABASE_URL = 'https://exgyekfwnztnaawaueuu.supabase.co'
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY
+const supabase = createClient( SUPABASE_URL,SUPABASE_ANON_KEY)
